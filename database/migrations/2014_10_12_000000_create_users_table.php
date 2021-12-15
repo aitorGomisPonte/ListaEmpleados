22b946a9c47ff485221375c3f8bca9e32958cab7
Aitor <aitor_enrique_gomis_tsapp1ma2021@cev.com>
2021-12-15 12:44:21 +0100

    Terminar log in, creacion de tokens

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();//Cuando confirmamos el mail
+            $table->string('password');//Hay que verificarla de alguna manera
             $table->string('puesto');
             $table->string('biografia');
             $table->string('api_token')->unique()->nullable();

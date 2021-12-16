e13528f33b8866fe2e9277a258c2c74f148d0005
Aitor <aitor_enrique_gomis_tsapp1ma2021@cev.com>
2021-12-16 12:24:44 +0100

    Programacion del middleware

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,39 @@ class CheckUser
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $respuesta = ["status" => 1,"msg" => ""];//Usamos esto para comunicarnos con el otro lado del servidor
+
+        $datos = $request->getContent(); //Nos recibimos los datos por el body
+        $datos = json_decode($datos); //Decodificamos el json para poder ver los distintos componentes
+        
+       try {
+          $token = $datos->api_token;
+          if($token){
+              $user = User::Where($token,"api_token")->first();
+              if($user){
+                  switch ($user->puesto) {
+                      case 'Directivo'||"RRHH":
+                          $permiso = true;
+                          break;  
+                      default:
+                          $permiso = false;
+                          break;
+                  }
+              }else{
+                $respuesta['msg'] = "El token no existe";
+                $respuesta['status'] = 0;
+              }
+          }
+       } catch (\Exception $e) {
+          $respuesta['msg'] = $e->getMessage();
+          $respuesta['status'] = 0;
+       }
+        if($permiso){
+            $respuesta['msg'] = "Permisos correctos";
+            $respuesta['status'] = 1;      
+            return $next($request);
+        }
+        return response()->json($respuesta);
+       
     }
 }
